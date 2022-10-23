1e2484cbb9ca4b72368e875b46d7e28610c3d274
dhruvchittamuri <dhruvchi@uw.edu>
2022-10-23 01:12:18 -0700

    Backend

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class NameForm extends React.Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            
+        }
+    }
+  }
+
+
